415d5789bcb7f1e5311aefe035740decfa954473
Azad MJ <azadmj@gmail.com>
2019-07-09 20:38:14 +0530

    initial commit

---
+++ trips/src/main/java/com/azad/trips/NextBus.java
@@ -0,0 +1,13 @@
+package com.azad.trips;
+
+/**
+ * Hello world!
+ *
+ */
+public class NextBus 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
