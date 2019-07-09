aeb8e4d844769de99d901282ef9c4e93d03a8b35
azadmj <azadmj@gmail.com>
2019-07-09 20:40:55 +0530

    updated - Docs

--- trips/src/main/java/com/azad/trips/NextBus.java
+++ trips/src/main/java/com/azad/trips/NextBus.java
@@ -1,13 +1,13 @@
 package com.azad.trips;
 
 /**
- * Hello world!
+ * Next Bus App - Rest Client for MetroTransit
  *
  */
 public class NextBus 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Next Bus App - Rest Client for MetroTransit" );
     }
 }
